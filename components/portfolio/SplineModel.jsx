aeff42f9a3208cde364be73bd9e5745d6ae98796
khaleddrashadd <khaledrashad111@gmail.com>
2023-08-31 20:14:34 +0300

    ❤

--- components/portfolio/SplineModel.jsx
+++ components/portfolio/SplineModel.jsx
@@ -5,7 +5,7 @@ import Spline from '@splinetool/react-spline';
 const SplineModel = () => {
   return (
     <div
-      className="relative"
+      className="relative mt-20"
       id="home">
       <Spline scene="https://prod.spline.design/1lk6XqjSrOY6s43U/scene.splinecode" />
       <div className="absolute bottom-10 w-full flex justify-center items-center">
